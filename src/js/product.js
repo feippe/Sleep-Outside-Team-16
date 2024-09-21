524599dff3a8c1749c6aab31def2f5d42c324bdc
fvalto <skualo_fv@msn.com>
2024-09-21 14:36:01 -0300

    fix cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let cart = getLocalStorage("so-cart");
+  if (cart == null) {
+    cart = []; // if cart empty, initialize as array
+  }
+  cart.push(product); //add product to cart
+  setLocalStorage("so-cart", cart); //update localstorage with current cart array
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
@@ -12,7 +17,7 @@ async function addToCartHandler(e) {
   addProductToCart(product);
 }
 
-// add listener to Add to Cart button
+// listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
