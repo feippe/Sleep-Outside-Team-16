0881d4366440daa18fcb31494f2d9af3e43267c0
Gabriel Feippe <gabriel@feippe.com>
2024-09-21 09:25:24 -0300

    Fix add to cart function

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let list = getLocalStorage("so-cart");
+  if (list == null) {
+    list = [];
+  }
+  list.push(product);
+  setLocalStorage("so-cart", list);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
