8622855373dae5d831c7f287df34f21431c8a998
Gabriel Feippe <gabriel@feippe.com>
2024-09-26 20:45:04 -0300

    Team work code

--- src/js/product.js
+++ src/js/product.js
@@ -1,6 +1,6 @@
-import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 
@@ -19,7 +19,40 @@ async function addToCartHandler(e) {
   addProductToCart(product);
 }
 
+function loadingDetailsToPage(details) {
+  document.querySelector("title").textContent =
+    `Sleep Outside | ${details.Name}`;
+  document.querySelector(".product-detail h3").textContent = details.Brand.Name;
+  document.querySelector(".product-detail h2").textContent =
+    details.NameWithoutBrand;
+  document
+    .querySelector(".product-detail img")
+    .setAttribute("src", details.Image);
+  document
+    .querySelector(".product-detail img")
+    .setAttribute("alt", details.Name);
+  document.querySelector(".product-detail .product-card__price").textContent =
+    `$${details.FinalPrice}`;
+  details.Colors.forEach((color) => {
+    let colorElement = document.createElement("span");
+    colorElement.textContent = color.ColorName;
+    document
+      .querySelector(".product-detail .product__color")
+      .appendChild(colorElement);
+  });
+  document.querySelector(".product-detail .product__description").innerHTML =
+    details.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").setAttribute("data-id", details.Id);
+}
+
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+
+const productId = getParam("product");
+if (productId !== undefined) {
+  const productDetails = new ProductDetails(productId, "tents");
+  await productDetails.init();
+  loadingDetailsToPage(productDetails.details);
+}
