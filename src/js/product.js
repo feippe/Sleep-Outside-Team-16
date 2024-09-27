a7196558e12dc2ba3b2646f19221caec3d7eaaea
Gabriel Feippe <gabriel@feippe.com>
2024-09-26 22:49:43 -0300

    improve code

--- src/js/product.js
+++ src/js/product.js
@@ -1,4 +1,4 @@
-import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, getParam, qs } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
@@ -19,30 +19,26 @@ async function addToCartHandler(e) {
   addProductToCart(product);
 }
 
-function loadingDetailsToPage(details) {
-  document.querySelector("title").textContent =
-    `Sleep Outside | ${details.Name}`;
-  document.querySelector(".product-detail h3").textContent = details.Brand.Name;
-  document.querySelector(".product-detail h2").textContent =
-    details.NameWithoutBrand;
-  document
-    .querySelector(".product-detail img")
-    .setAttribute("src", details.Image);
-  document
-    .querySelector(".product-detail img")
-    .setAttribute("alt", details.Name);
-  document.querySelector(".product-detail .product-card__price").textContent =
+async function loadingDetailsToPage(id) {
+  const productDetails = new ProductDetails(id, "tents");
+  await productDetails.init();
+  const details = productDetails.details;
+
+  qs("title").textContent = `Sleep Outside | ${details.Name}`;
+  qs(".product-detail h3").textContent = details.Brand.Name;
+  qs(".product-detail h2").textContent = details.NameWithoutBrand;
+  qs(".product-detail img").setAttribute("src", details.Image);
+  qs(".product-detail img").setAttribute("alt", details.Name);
+  qs(".product-detail .product-card__price").textContent =
     `$${details.FinalPrice}`;
   details.Colors.forEach((color) => {
     let colorElement = document.createElement("span");
     colorElement.textContent = color.ColorName;
-    document
-      .querySelector(".product-detail .product__color")
-      .appendChild(colorElement);
+    qs(".product-detail .product__color").appendChild(colorElement);
   });
-  document.querySelector(".product-detail .product__description").innerHTML =
+  qs(".product-detail .product__description").innerHTML =
     details.DescriptionHtmlSimple;
-  document.querySelector("#addToCart").setAttribute("data-id", details.Id);
+  qs("#addToCart").setAttribute("data-id", details.Id);
 }
 
 // add listener to Add to Cart button
@@ -50,9 +46,7 @@ document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
 
-const productId = getParam("product");
+let productId = getParam("product");
 if (productId !== undefined) {
-  const productDetails = new ProductDetails(productId, "tents");
-  await productDetails.init();
-  loadingDetailsToPage(productDetails.details);
+  loadingDetailsToPage(productId);
 }
